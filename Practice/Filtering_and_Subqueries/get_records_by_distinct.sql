da2279bebe13d4c75ef520bda262a307b135c11d
BSesha <subbu.cva@gmail.com>
2025-05-10 20:29:45 +0530

    Create get_records_by_distinct.sql
    Retrieve all unique customer IDs from the customer_purchases table

---
+++ Practice/Filtering_and_Subqueries/get_records_by_distinct.sql
@@ -0,0 +1,9 @@
+/* Retrieve all unique customer IDs from the customer_purchases table */
+
+SELECT DISTINCT customer_id 
+FROM farmers_market.customer_purchases;
+
+/*
+* Explanation:
+* DISTINCT: Ensures only unique customer IDs are returned.
+*/
