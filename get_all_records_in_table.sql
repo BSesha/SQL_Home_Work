813f4bfada2f1b4ae6b5eff8aceb43f775830ece
BSesha <subbu.cva@gmail.com>
2025-05-10 19:25:10 +0530

    Create get_all_records_in_table.sql
    Get all the products available in the market

---
+++ get_all_records_in_table.sql
@@ -0,0 +1,9 @@
+/* Get all the products available in the market */
+
+SELECT * FROM farmers_market.product;
+
+/*
+*   Explanation:
+*   The SELECT * statement retrieves all columns from the product table in the farmers_market schema.
+*   While using * is convenient, it is recommended to explicitly list column names to avoid issues when new columns are added or modified.
+*/
